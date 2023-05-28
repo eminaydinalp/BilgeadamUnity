322054111d7508caa7501c5bd5bae842e865a74b
eminaydinalp <muhammetemin4444@gmail.com>
2023-05-28 15:18:48 +0300

    Odev

---
+++ Assets/Scripts/Odev1/Odev1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Odev1 : MonoBehaviour
+{
+    //     Soru1- Bir cube oluşturup içine attığımız bir script ile cube’nin boxcolliderinin isTrigger değerini true, radius değerini 2 yapınız ve bu değerleri consola yazdırınız.
+    //
+    //     Soru2- Soru1 deki objeye bir Rigidbody componenti ekleyip aynı scriptten bu Rigidbody compenentine ulaşıp mass değerini 10, drag değerini 5 yapınız ve konsolda gösteriniz.
+    //
+    //     Soru3- Yeni bir obje oluşturup bu objenin altına 10 adet child obje oluşturunuz.
+    //      Child objelerimize rigidbody ekleyelim ve parent objemize attığımız script ile child objelerimizdeki rigidbodyleri bir dizinin içine alalım.
+    //      Dizideki bütün rigidbodylerin usegravity değerini false yapalım.
+    //
+    //     Soru4- 2 ayrı objemiz olsun. Birinde bizim yazdığımız bir script olsun diğerinde ise SphereCollider olsun.
+    //      Kendi yazdığımız script ile diğer objede var olan SphereCollidera ulaşıp isTrigger değerini true, radius değerini 3 yapalım.
+    //
+    //     Soru5- 2 ayrı objemiz ve her ikisine attığımız 2 ayrı scriptimiz olsun.
+    //      Bu iki scripte int, string ve bool değişkenler ve bu değişkenlerle alakalı metot oluşturalım.
+    //      En son bu objelerden birbirine ve birbirinin içindeki scriptlere ulaşıp değişkenlerin değerlerini değiştirelim.
+    //      Ve metotlarını kullanıp consola değiştirdiğimiz değişkenleri yazalım.
+    //  (Burda size bırakıyorum scriptleri nasıl oluşturacağınızı vs)
+}
