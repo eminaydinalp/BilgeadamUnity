d08a89eb942eb44586ea2763d95cffacda4098fc
eminaydinalp <muhammetemin4444@gmail.com>
2023-06-03 11:03:50 +0300

    Github New

--- Assets/Scripts/GithubOrnek.cs
+++ Assets/Scripts/GithubOrnek.cs
@@ -7,7 +7,7 @@ public class GithubOrnek : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Github Örnek");
     }
 
     // Update is called once per frame
